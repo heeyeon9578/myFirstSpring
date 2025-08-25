3d3429e92b90990097e1bc5a9dd0d98a2f1dc24c
heeyeon9578 <heeyeon9578@naver.com>
2025-08-26 01:17:45 +0900

    feat: 회원조회, 회원추가 로직 구성

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,54 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member){
+
+        //중복이름 방지
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+         .ifPresent(m->{
+             throw new IllegalStateException("이미 존재하는 회원입니다.");
+         });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
